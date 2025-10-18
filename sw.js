d60e019f30f4e912128782f0306102823d6ba9bc
Guilherme Medeiros <guilhermemedeiros01844@gmail.com>
2025-10-17 22:09:17 -0300

    PWA Test

---
+++ sw.js
@@ -0,0 +1,19 @@
+// Service Worker básico
+const CACHE_NAME = 'morfologic-v1';
+
+self.addEventListener('install', (event) => {
+  console.log('✅ Service Worker instalado');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['/', '/index.html']);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
